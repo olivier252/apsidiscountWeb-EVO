0091c628df7131cc5baf3c0c39c4e783ae5e8509
02victor04 <victor.breton02@gmail.com>
2019-07-22 10:28:07 +0200

    fix and modified dao and service for category and manufacturer

--- src/main/java/com/apsidiscount/controller/CategoryRestController.java
+++ src/main/java/com/apsidiscount/controller/CategoryRestController.java
@@ -4,17 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.apsidiscount.entity.Categorie;
 import com.apsidiscount.service.CategoryService;
 
+@RestController
 public class CategoryRestController {
 
 	@Autowired
 	private CategoryService categoryService;
 	
 	//Ajout 21/7
-	@GetMapping(produces = "application/json", path="/api/allcatyegory")
+	@GetMapping(produces = "application/json", path="/api/allcategory")
 	public List<Categorie> getAllCategories() {
 	return  categoryService.getAllCat();
 	}
--- src/main/java/com/apsidiscount/controller/ConstructorRestController.java
+++ src/main/java/com/apsidiscount/controller/ConstructorRestController.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.apsidiscount.entity.Constructeur;
 import com.apsidiscount.service.ConstructorService;
 
+@RestController
 public class ConstructorRestController {
 	@Autowired
 	private ConstructorService constructorService;
 	
 	//Ajout 21/7
-	@GetMapping(produces = "application/json", path="/api/allconstructors")
+	@GetMapping(produces = "application/json", path="/api/allmanufacturer")
 	public List<Constructeur> getAllConstruct() {
 	return  constructorService.getAllConstructors();
 	}
